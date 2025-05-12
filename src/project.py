3e436e0d4b49ab673d5bcc07d57fea6e7dd7c705
Ness T <vxt240003@utdallas.edu>
2025-05-12 18:50:21 -0500

    despawn passed obstacles

--- src/project.py
+++ src/project.py
@@ -13,7 +13,8 @@ def create_pipe():
 def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx -= 5
-    return pipes
+    visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+    return visible_pipes
 
 def draw_pipes(pipes):
     for pipe in pipes:
@@ -24,12 +25,15 @@ def draw_pipes(pipes):
             screen.blit(flip_pipe,pipe)
 
 def check_collision(pipes):
+    global can_score
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
             death_sound.play()
+            can_score = True
             return False
 
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        can_score = True
         return False
 
     return True
@@ -62,18 +66,32 @@ def update_score(score, high_score):
 		high_score = score
 	return high_score
 
+def pipe_score_check():
+     global score, can_score
+
+     if pipe_list:
+            for pipe in pipe_list:
+                if 95 < pipe.centerx < 105 and can_score:
+                    score += 1
+                    score_sound.play()
+                    can_score = False
+                if pipe.centerx < 0:
+                    can_score = True
+                
 
 pygame.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=512)
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
 game_font = pygame.font.Font('Boxy-Bold.ttf',40)
+
 #game Variables
 gravity = 0.25
 bird_movement = 0
 game_active = True
 score = 0
 high_score = 0
+can_score = True
 
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
@@ -110,6 +128,7 @@ game_over_rect = game_over_surface.get_rect(center = (288,512))
 flap_sound = pygame.mixer.Sound('sound/sfx_wing.wav')
 death_sound = pygame.mixer.Sound('sound/sfx_hit.wav')
 score_sound = pygame.mixer.Sound('sound/sfx_point.wav')
+score_sound_countdown = 100
 
 
 while True:
@@ -145,17 +164,20 @@ while True:
     screen.blit(bg_surface,(0,0))
 
     if game_active:
+        # Bird
         bird_movement += gravity
         rotated_bird = rotate_bird(bird_surface)
         bird_rect.centery += bird_movement
         screen.blit(rotated_bird,bird_rect)
         game_active = check_collision(pipe_list)
-
+        # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
 
-        score += 0.01
+        #Score
+        pipe_score_check()
         score_display('main_game')
+       
     else:
         screen.blit(game_over_surface,game_over_rect)
     
