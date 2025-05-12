afe737c9804109f8beb2a83b1bf3bcc8e4eabc20
Ness T <vxt240003@utdallas.edu>
2025-05-12 16:33:08 -0500

    obstacle animation implimented

--- src/project.py
+++ src/project.py
@@ -3,6 +3,19 @@ import pygame, sys
 def draw_floor():
 	screen.blit(floor_surface,(floor_x_pos,900))
 	screen.blit(floor_surface,(floor_x_pos + 576,900))
+    
+def create_pipe():
+     new_pipe = pipe_surface.get_rect(midtop = (700,512))
+     return new_pipe
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        screen.blit(pipe_surface,pipe)
 
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
@@ -24,6 +37,8 @@ bird_rect = bird_surface.get_rect(center = (100,512))
 pipe_surface = pygame.image.load('assets/pipe-green.png').convert()
 pipe_surface = pygame.transform.scale2x(pipe_surface)
 pipe_list = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE,1200)
 
 while True:
     for event in pygame.event.get():
@@ -34,12 +49,19 @@ while True:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 12
+        if event.type == SPAWNPIPE:
+            pipe_list.append(create_pipe())
 
     screen.blit(bg_surface,(0,0))
 
     bird_movement += gravity
     bird_rect.centery += bird_movement
     screen.blit(bird_surface,bird_rect)
+
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
+
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -576:
