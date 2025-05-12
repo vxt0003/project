d417b8ef0d86d0b28d3ad00aa7478b94b9469549
Ness T <vxt240003@utdallas.edu>
2025-05-12 14:41:19 -0500

    added bg picture

--- project.py
+++ project.py
@@ -3,11 +3,16 @@ pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
 
+bg_surface = pygame.image.load('assets/background-day.png')
+bg_surface = pygame.transform.scale2x(bg_surface)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.blit(0,0)
 
 
 
