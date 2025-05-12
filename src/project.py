0964ef480c1cb4d17d27e8b04ea1eb756bccf643
Ness T <vxt240003@utdallas.edu>
2025-05-12 17:04:51 -0500

    collisions responsive

--- src/project.py
+++ src/project.py
@@ -17,13 +17,29 @@ def move_pipes(pipes):
 
 def draw_pipes(pipes):
     for pipe in pipes:
-        screen.blit(pipe_surface,pipe)
+        if pipe.bottom >= 1024:
+            screen.blit(pipe_surface,pipe)
+        else:
+            flip_pipe = pygame.transform.flip(pipe_surface,False,True)
+            screen.blit(flip_pipe,pipe)
+
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+
+    if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        return False
+
+    return True
 
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
+#game Variables
 gravity = 0.25
 bird_movement = 0
+game_active = True
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -49,20 +65,30 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement -= 12
+            if event.key == pygame.K_SPACE and game_active == False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100,512)
+                bird_movement = 0
+
+        
+
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
 
     screen.blit(bg_surface,(0,0))
 
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird_surface,bird_rect)
+    if game_active:
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird_surface,bird_rect)
+        game_active = check_collision(pipe_list)
 
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
 
 
     floor_x_pos -= 1
