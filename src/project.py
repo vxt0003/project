d73b4b2d4b03db872d85172fb11398db7d363a53
Ness T <vxt240003@utdallas.edu>
2025-05-12 16:01:47 -0500

    adding ser input and character

--- src/project.py
+++ src/project.py
@@ -7,6 +7,8 @@ def draw_floor():
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
+gravity = 0.25
+bird_movement = 0
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -15,14 +17,25 @@ floor_surface = pygame.image.load('assets/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
+bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
+bird_surface = pygame.transform.scale2x(bird_surface)
+bird_rect = bird_surface.get_rect(center = (100,512))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 12
 
     screen.blit(bg_surface,(0,0))
-  
+
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+    screen.blit(bird_surface,bird_rect)
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -576:
