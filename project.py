446a24a9677e8ad79dda9685ff6e31d540b109ac
Ness T <vxt240003@utdallas.edu>
2025-05-12 13:21:55 -0500

    window screen with exit button

---
+++ project.py
@@ -0,0 +1,14 @@
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode((576,1024))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+
+    pygame.display.update()
+   
